54f32e6814b90a274afb61ceb94447b46cb87c14
Jake Bian <jake@getalkanet.com>
2016-07-11 17:44:54 -0700

    dev: update python package dependencies

--- spy/setup.py
+++ spy/setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
-setup(name="heraspy",
-      version="0.0",
-      description="Keras data extraction callbacks",
-      author="Jake Bian",
-      author_email='jake@getalkanet.com',
-      platforms=["any"],
-      license="wtfpl",
-      packages=find_packages(),
-      )
+setup(
+    name='heraspy',
+    version='0.0',
+    description='Keras data extraction callbacks',
+    author='Jake Bian',
+    author_email='jake@getalkanet.com',
+    platforms=['any'],
+    license='wtfpl',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'socketIO-client',
+        'keras'
+    ],
+)
